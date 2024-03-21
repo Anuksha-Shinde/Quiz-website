c4750d043f7617b3688f74c0a20482f1b207478c
Anuksha-Shinde <164325044+Anuksha-Shinde@users.noreply.github.com>
2024-03-21 09:56:52 +0100

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,128 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "Which of the following best describes virtual commissioning?",
+    answer: "Simulating and testing control software and devices before connecting them to the real system",
+    options: [
+      "Testing control software in a physical environment",
+      "Simulating and testing control software and devices before connecting them to the real system",
+      "Designing control software for virtual reality systems",
+      "Installing virtual machines for industrial automation"
+    ]
+  },
+    {
+    numb: 2,
+    question: "What is the main function for RF:ViPer in virtual commissioning?",
+    answer: "Programming function blocks and simulating periphery devices",
+    options: [
+      "Creating 3D visualizations",
+      "Programming virtual machine models",
+      "Simulating control software",
+      "Programming function blocks and simulating periphery devices"
+    ]
+  },
+    {
+    numb: 3,
+    question: "What is the primary purpose of a virtual robot controller?",
+    answer: "Debugging robot projects virtually",
+    options: [
+      "Debugging robot projects virtually",
+      "Controlling physical robots in real-time",
+      "Designing robot hardware components",
+      "Simulating virtual reality environments"
+    ]
+  },
+    {
+    numb: 4,
+    question: "What is RF::YAMS® used for in virtual commissioning?",
+    answer: "Preparing and simulating 3D stations",
+    options: [
+      "Creating physical prototypes",
+      "Simulating 2D environments",
+      "Preparing and simulating 3D stations",
+      "Testing electrical circuits"
+    ]
+  },
+    {
+    numb: 5,
+    question: "How is RF::ViPer® conneted to the RF::Suite system for virtual commissioning?",
+    answer: "Through shared memory",
+    options: [
+      "Through Wi-Fi connection",
+      "Through Bluetooth connection",
+      "Through shared memory",
+      "Through a physical cable connection"
+    ]
+  },
+    {
+    numb: 6,
+    question:"How are function bLocks created and used in RF::ViPer®?",
+    answer: "They are created with periphery element behaviour logic and can be saved in standard libraries for reuse",
+    options: [
+        "They are imported from CAD software",
+        "They are generated automatically",
+        "They are predefined templates without customization options",
+        "They are created with periphery element behaviour logic and can be saved in standard libraries for reuse"
+    ]
+  },
+    {
+    numb: 7,
+    question:"What type of data does RF::RobSim® support for simulation?",
+    answer: "Kinematics data and robot programs",
+    options: [
+        "CAD models",
+        "Kinematics data and robot programs",
+        "Electrical schematics",
+        "Process flow diagrams"
+    ]
+  },
+    {
+    numb: 8,
+    question:"Which tool is used to create logic and simulate periphery devices in virtual systems during virtual commissining?",
+    answer: "RF::ViPer",
+    options: [
+        "RF::YAMS®",
+        "RF::SCOUT®",
+        "RF::ViPer",
+        "RF::FSBOX®"
+    ]
+  },
+  {
+    numb: 9,
+    question:"What types of projects are supported by RF::YAMS®?",
+    answer: "Projects developed from Process Simulate, DELMIA, and other 3D tools",
+    options: [
+        "Projects developed from Process Simulate, DELMIA, and other 3D tools",
+        "2D Projects only",
+        "Projects developed from CAD software",
+        "Projects developed from Microsoft Office Suite"
+    ]
+  },
+  {
+    numb: 10,
+    question:"Why is connecting RF::YAMS® with RF::ViPer® significant in virtual commissioing?",
+    answer: "It ensures accurate simulation and testing of control systems and periphery components",
+    options: [
+        "It increases processing speed",
+        "It allows for the creation of virtual reality environments",
+        "It reduces the cost of software licenses",
+        "It ensures accurate simulation and testing of control systems and periphery components"
+    ]
+  }
+  
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
